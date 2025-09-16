be288bca43f107820b01ce2497edf819d19bc7e1
wadesinclairjr <wadesinclairjr@gmail.com>
2025-09-16 14:09:33 -0400

    quiz_lesson1_q3.py
    Added Lesson 1 Quiz Q3

---
+++ quiz_lesson1_q3.py
@@ -0,0 +1,8 @@
+username = "Wade"
+age = 34
+rate = 20.5
+is_beginner = True
+print(f"Username: {username}, Type: {type(username)}")
+print(f"Age: {age}, Type: {type(age)}")
+print(f"Rate: {rate}, Type: {type(rate)}")
+print(f"Beginner: {is_beginner}, Type: {type(is_beginner)}")
