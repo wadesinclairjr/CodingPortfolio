bff938451004000a4893c768c8e5b01f528912be
wadesinclairjr <wadesinclairjr@gmail.com>
2025-09-17 09:49:26 -0400

    quiz_lesson3.py

---
+++ quiz_lesson3.py
@@ -0,0 +1,27 @@
+# Quiz Q1
+for i in range(1, 5):
+    print(i)
+
+ # Quiz Q2
+x = 1
+while x <= 3:
+    print(f"Retry {x}")
+    x += 1   
+
+# Quiz Q3
+for i in range(0, 6, 2):
+    print(i)
+
+# Quiz Q4
+for i in range(1, 6):
+    if i % 2 != 0:
+        print(f"{i}: Active user")
+
+# Quiz Q5
+num = 1
+while num <= 5:
+    if num == 3:
+        num += 1
+        continue
+    print(num)
+    num += 1
